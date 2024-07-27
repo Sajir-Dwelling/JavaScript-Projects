1f6d4557d37d6f8779271a99b9d45d8e540d3f38
Sajir-Dwelling <sifaz20075@gmail.com>
2024-07-27 14:27:51 +0600

    Add files via upload

---
+++ constructiveFunction.js
@@ -0,0 +1,7 @@
+//Constructive Function.
+function brothers(name, age, student, languages) {
+    this.name = name;
+    this.age = age;
+    this.student = student;
+    this.languages = languages;
+}
