1927de8bd0f8a4e9a794fb1a13228b184d81223f
Sajir-Dwelling <sifaz20075@gmail.com>
2024-07-29 14:32:25 +0600

    Add files via upload

---
+++ Bulb/bulb.js
@@ -0,0 +1,8 @@
+function bulb(){
+    var bulboff = document.querySelector(".off");
+    bulboff.setAttribute("src","lite.png");
+}
+function bulb1(){
+    var bulboff = document.querySelector(".off");
+    bulboff.setAttribute("src","liteoff.png");
+}
